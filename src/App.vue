cbd2f1e8dbd65e74aaeee8599cb8160db818c490
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-02-28 16:15:25 +0900

    Install vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <router-link to='/'>top</router-link>
+    <router-link to='books'>books</router-link>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const path =  require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+    publicPath: '/',
+  },
+  resolve: {
+    modules: [
+      path.resolve('./src'),
+      path.resolve('./node_modules'),
+    ],
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+    ],
+  },
+}

f5b67c174b86092b9c06d8fc3c5a714ef270c6fc
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-02-28 23:42:39 +0900

    Add production mode for build

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
 const path =  require('path')
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV || 'development',
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
